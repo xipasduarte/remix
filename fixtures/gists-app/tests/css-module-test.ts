b45ff5c1f70a0b0fe82e884daa0a2ccb84adebad
Chance Strickland <hi@chance.dev>
2022-01-05 21:38:19 -0800

    chore: test placeholder

---
+++ fixtures/gists-app/tests/css-module-test.ts
@@ -0,0 +1,26 @@
+// import type { Browser, Page } from "puppeteer";
+// import puppeteer from "puppeteer";
+// import * as Utils from "./utils";
+
+// const testPort = 3000;
+// const testServer = `http://localhost:${testPort}`;
+
+export {};
+
+describe("CSS module import", () => {
+  //   let browser: Browser;
+  //   let page: Page;
+  //   beforeEach(async () => {
+  //     browser = await puppeteer.launch();
+  //     page = await browser.newPage();
+  //   });
+
+  //   afterEach(() => browser.close());
+
+  it.todo("uses the hashed classname", async () => {
+    // await page.goto(`${testServer}/one`);
+    // await Utils.reactIsHydrated(page);
+    // await page.waitForSelector('[data-test-id="counter-button"]');
+    // TODO: Get the classname and ensure it has a hash
+  });
+});
